dee965419f6a46bfda34ee75502d2a825363336c
J-Haze <59891689+J-Haze@users.noreply.github.com>
2020-09-27 19:36:09 -0700

    Updates

---
+++ precache-manifest.c896b9996e73545a1f21f3d685775dcd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d00c0d863d44519ae88c77dcc25ad68",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "f77a99e42a473949e76d",
+    "url": "/Weather/static/css/2.0b7aed13.chunk.css"
+  },
+  {
+    "revision": "66da3a6ba63a1ff5254f",
+    "url": "/Weather/static/css/main.a6eb3cf1.chunk.css"
+  },
+  {
+    "revision": "f77a99e42a473949e76d",
+    "url": "/Weather/static/js/2.52c474ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.52c474ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66da3a6ba63a1ff5254f",
+    "url": "/Weather/static/js/main.449c1482.chunk.js"
+  },
+  {
+    "revision": "ccb46818c524438dfc04",
+    "url": "/Weather/static/js/runtime-main.6a98160b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Weather/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Weather/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Weather/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Weather/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Weather/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "be7f57bf0ed6cbcf5aa24809e1d1d873",
+    "url": "/Weather/static/media/humidity.be7f57bf.png"
+  },
+  {
+    "revision": "0abde9ff0a94750dec91d1a7ca0cf16d",
+    "url": "/Weather/static/media/pressure.0abde9ff.png"
+  }
+]);
